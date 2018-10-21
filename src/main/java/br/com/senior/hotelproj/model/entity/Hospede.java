d9dcb848ba4425bb1e833dc91a142093598eed99
igor1983rodrigues <igor.ti.1983@gmail.com>
2018-10-20 22:41:16 -0300

    tratando a entidade Hospede

--- src/main/java/br/com/senior/hotelproj/model/entity/Hospede.java
+++ src/main/java/br/com/senior/hotelproj/model/entity/Hospede.java
@@ -1,5 +1,22 @@
 package br.com.senior.hotelproj.model.entity;
 
-public class Hospede {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "TB_HOSPEDE")
+public class Hospede {
+	@Id
+	@Column(name = "ID_HOSPEDE")
+	private int idHospede;
+	
+	public int getIdHospede() {
+		return this.idHospede;
+	}
+	
+	public void setIdHospede(int idHospede) {
+		this.idHospede = idHospede;\F
+	}
 }
